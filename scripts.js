bf8a14312c17ff3b7f6284edac987bca7fbcfe4c
Kakashi <bschindlerm@gmail.com>
2024-09-13 15:42:33 -0300

    Cambiar varias cosaas aaa

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-// Modal window
+
 
 // JS to show lawyers new window on click
 const modal = document.getElementById("team-modal");
@@ -51,31 +51,31 @@ const closeBtn = document.querySelector(".close");
 
 const teamData = {
   jose: {
-    name: "José Antonio Abarca",
-    position: "Socio Fundador",
-    email: "jabarca@abarcazanartu.cl",
-    phone: "+56 9 6657 8170",
-    linkedin: "https://www.linkedin.com/in/josé-antonio-abarca-193173304/",
-    description: "Abogado Universidad Diego Portales (1997) Abogado, con más de 25 años de ejercicio libre de la profesión. En el ámbito privado, socio fundador de Estudio Jurídico Abarca & Abogados, que se transformó a Abarca Zañartu Abogados desde 2022. Estudio que asesora a empresas de diversos ámbitos de carácter civil. Funcionario Juzgado de Policía Local de Recoleta (1°) y Pudahuel (1°) desde 1998 a 2006.- Desempeñándose, además, como Juez Arbitro, desde el periodo 2017 a la fecha, nombrado por la Ilustrísima Corte de Apelaciones de Santiago. Mediador Familiar.",
-    image: "../images/jose.jpg"
+    name: "Andrés Heerlein Lobenstein",
+    position: "Psiquiatra",
+    email: "ajheerlein@gmail.com",
+    phone: "",
+    linkedin: "https://www.linkedin.com/in/andr%C3%A9s-heerlein-lobenstein-46257a7a/",
+    description: "Psiquiatra",
+    image: "../images/andres-portrait.jpeg"
   },
   beltran: {
-    name: "Beltrán Zañartu Urbina",
-    position: "Socio",
-    email: "bzanartu@abarcazanartu.cl",
-    phone: "+56 9 9221 4351",
-    linkedin: "https://www.linkedin.com/in/beltrán-francisco-zañartu-urbina-445731140/",
-    description: "Abogado Universidad de Chile (2016). Especialista en Derecho Ambiental. Beltrán comenzó su carrera en la Superintendencia de Servicios Sanitarios, para luego desempeñarse en los estudios jurídicos VGC Abogados y Eelaw Medio Ambiente y Energía. Forma parte del equipo de Abarca Zañartu Abogados desde marzo del 2022, estableciéndose como socio en junio de 2023. Beltrán es también Mediador Familiar de la Universidad Diego Portales y director de la Fundación Redes en Cajas.",
-    image: "../images/bel.jpg"
+    name: "Estrella Muñoz",
+    position: "Psicóloga",
+    email: "mail",
+    phone: "phone",
+    linkedin: "",
+    description: "Psicóloga",
+    image: "../images/estrella.jpg"
   },
   bianca: {
-    name: "Bianca Barrueto Zamora",
-    position: "Asociada",
-    email: "bbarrueto@abarcazanartu.cl",
-    phone: "+56 9 6247 3575",
-    linkedin: "https://www.linkedin.com/in/bianca-barrueto-zamora-985484ab/",
-    description: "Bianca Isa Barrueto Zamora abogada Universidad Central de Chile (2010). Mediadora Familiar, Universidad Católica de Chile. Abogada, con más de 12 años de experiencia en el ámbito público y privado. Cofundadora y Abogada corporativa de Bike Santiago, proyecto desarrollado por Banco Itaú, Empresa Trek (Waterloo USA) y distintas municipalidades de la ciudad de Santiago (2012-2017). Funcionaria Pública, de la Municipalidad de Lo Barnechea y San Miguel, desempeñándose como abogada en Secretaría Municipal y Dirección Jurídica (2017-2020). Bianca se unió al equipo de Abarca Zañartu como Abogada Asociada en agosto de 2024.",
-    image: "../images/bianca.jpeg",
+    name: "Daniela",
+    position: "Psicóloga",
+    email: "mail",
+    phone: "",
+    linkedin: "",
+    description: "Psicóloga",
+    image: "../images/daniela.jpg",
   }
 };
 
